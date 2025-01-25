220275f6297f5bc5100a3e2a857a371f894fdc18
CrisArMnz <114408747+CrisArMnz@users.noreply.github.com>
2025-01-25 05:41:10 -0300

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -13,6 +13,36 @@ CWD = os.path.abspath(os.path.dirname(__file__))
 os.chdir(CWD)
 sys.path.append(CWD)
 
+def actualizar_base_datos():
+    try:
+        with open("./assets/app_settings.yaml", 'r', encoding='utf-8') as file:
+            app_settings = yaml.safe_load(file)
+
+        data = DataDawnLoader(app_settings["DATA"])
+        process = DataProcess(app_settings)
+
+        df = pd.read_parquet(app_settings["PROCESS"]["OUT_DIR"] + app_settings["PROCESS"]["OUT_FILE_NAME"])
+        current_week = int(datetime.datetime.now().isocalendar().week)
+        current_year = int(datetime.datetime.now().isocalendar().year)
+        df = df[df["Fecha_Semana"].dt.isocalendar().year == current_year]
+
+        if not df.empty:
+            last_week = pd.to_datetime(df["Fecha_Semana"]).dt.isocalendar().week.max()
+            last_year = pd.to_datetime(df["Fecha_Semana"]).dt.isocalendar().year.max()
+            print(f"Ultima semana del año descargada {last_week} - semana actual {current_week}")
+            if (current_week > last_week) or (last_year < current_year):
+                process.get_filter_data(force_process=False)
+                data.update_current_year_data()
+                process.update_data()
+            else:
+                print("DATOS ESTAN ACTUALIZADOS")
+        else:
+            process.get_filter_data(force_process=False)
+            data.update_current_year_data()
+            process.update_data()
+    except Exception as e:
+        print(f"Error al actualizar la base de datos: {e}")
+
 if __name__ == '__main__':
     with open("./assets/app_settings.yaml", 'r', encoding='utf-8') as file:
         app_settings = yaml.safe_load(file)
